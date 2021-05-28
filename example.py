1485382d7911401ee648837e660c7fd8553adbc2
Scott Russell <me@scottrussell.net>
2021-05-27 19:03:42 -0500

    refactoring

--- example.py
+++ example.py
@@ -5,6 +5,7 @@ from grimoire import Page
 
 if __name__ == '__main__':
 
+    # define pages
     first = Page(
         text='Choose your own adventure!'
     )
@@ -12,22 +13,26 @@ if __name__ == '__main__':
     class Second(Page):
         text = 'You chose {choice}!'
 
+    
     rafting = Second(choice='Whitewater rafting')
     climbing = Second(choice='Climb Mount Everest')
     moon = Second(choice='Take a rocket to the moon')
 
-    first.option('Go Whitewater rafting', rafting)
-    first.option('Climb Mount Everest', climbing)
-    first.option('To the moon', moon)
     
     third = Page(
         text='You still need to do {choice}'
     )
 
+
+    # hook'em up
+    first.option('Go Whitewater rafting', rafting)
+    first.option('Climb Mount Everest', climbing)
+    first.option('To the moon', moon)
     rafting.option('what to do next?', third)
     climbing.option('what to do next?', third)
     moon.option('what to do next?', third)
-    
     third.option('home', first)
 
+
+    ## render
     first.render()
--- grimoire/core.py
+++ grimoire/core.py
@@ -25,27 +25,34 @@ class Page:
     def option(self, option_text, page):
         page.option_text = option_text
         self.options.append(page)
+    
+    def template(self, text, option_links):
+        return Doc(
+            Html(
+                Body(
+                    P(text),
+                    Ul(*[Li(A(o.option_text, href=f'{l}.html')) for l, o in option_links])
+                )
+            )     
+        )
 
-    def render(self, start=True, **state):
+    def render(self, updater=None, start=True, **state):
         if start:
             for root, _, files in os.walk("build"):
                 for file in files:
                     os.remove(os.path.join(root, file))
+
         new_state = {**state, **self.state}
-        formatted_text = self.text.format(**new_state)
+        if updater:
+            new_state = updater(new_state)
+        
         page_hash = f'{abs(int(hash(str(self.id) + str(new_state))))}'
         is_cached = page_hash in self._cache
         self._cache.append(page_hash)
         if not is_cached:
             hashes = [(o.render(**new_state, start=False), o) for o in self.options]
-            content = Doc(
-                Html(
-                    Body(
-                        P(formatted_text),
-                        Ul(*[Li(A(o.option_text, href=f'{h}.html')) for h, o in hashes])
-                    )
-                )     
-            )
+            formatted_text = self.text.format(**new_state)
+            content = self.template(formatted_text, hashes)
             filename = 'index.html' if start else f'{page_hash}.html'
             with open(f'build/{filename}', 'w') as f:
                 f.write(str(content))
