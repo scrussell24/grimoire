53f5314dc34d4ca26039feb49a140a6010912a74
Scott Russell <scrussell24@gmail.com>
2022-01-11 23:05:43 -0600

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="grimoire-if",
-    version="1.0.2",
+    version="1.0.3",
     install_requires=["hype-html"],
     author="Scott Russell",
     author_email="me@scottrussell.net",
