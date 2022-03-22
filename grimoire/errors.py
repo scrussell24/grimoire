00147ec56124a7528fa3fb85368568005a193b26
Scott Russell <me@scottrussell.net>
2022-03-21 20:07:31 -0500

    add a space

--- grimoire/errors.py
+++ grimoire/errors.py
@@ -2,5 +2,5 @@ class GrimoireInvalidOptionError(Exception):
     def __init__(self, option):
         super().__init__(
             f"Option ({option.__name__}) is a function. Remember to include"
-            "the option as an argument to your page function"
+            " the option as an argument to your page function"
         )
