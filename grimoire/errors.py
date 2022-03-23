17f78512fe0615b382b1aba8dab0cbecfea8cd63
Scott Russell <me@scottrussell.net>
2022-03-22 19:20:29 -0500

    oops, commit error

--- grimoire/errors.py
+++ grimoire/errors.py
@@ -12,3 +12,11 @@ class GrimoireUnknownPageOptions(Exception):
             f"Unknown page options: {options}. Make sure the options match the "
             " name of an existing page function."
         )
+
+
+class GrimoireNoStartPage(Exception):
+    def __init__(self):
+        super().__init__(
+            f"No start page was added. Remember to include the keyword arg"
+            " start=True to your stories first page."
+        )
